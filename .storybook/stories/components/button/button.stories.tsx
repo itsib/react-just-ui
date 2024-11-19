615754c3a12528ddee5502354d9f5f5fec6b86b0
Sergey Krainov <itsib.su@gmail.com>
2024-11-19 12:57:07 +0700

    Add btn prefix

--- .storybook/stories/components/button/button.stories.tsx
+++ .storybook/stories/components/button/button.stories.tsx
@@ -62,35 +62,35 @@ export const Basic: Story = {
 
         <div className="header">Outline</div>
 
-        <button className={`btn btn-primary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-primary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Primary</span>
         </button>
 
-        <button className={`btn btn-outline-primary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-outline-primary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Primary</span>
         </button>
 
-        <button className={`btn btn-accent w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-accent w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Accent</span>
         </button>
 
-        <button className={`btn btn-outline-accent w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-outline-accent w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Accent</span>
         </button>
 
-        <button className={`btn btn-secondary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-secondary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Secondary</span>
         </button>
 
-        <button className={`btn btn-outline-secondary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-outline-secondary w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Secondary</span>
         </button>
 
-        <button className={`btn btn-danger w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-danger w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Danger</span>
         </button>
 
-        <button className={`btn btn-outline-danger w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
+        <button className={`jj-btn btn-outline-danger w-full ${loading ? 'loading' : ''}`} disabled={disabled}>
           <span>Danger</span>
         </button>
       </section>
